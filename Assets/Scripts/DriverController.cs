fd45fd5f901e45b7a12d5c41f60f5228f6498fad
LiamWils20 <90903494+cmoussa4@users.noreply.github.com>
2023-11-15 15:56:52 +0000

    Simple Menu

--- Assets/Scripts/DriverController.cs
+++ Assets/Scripts/DriverController.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class DriverController : MonoBehaviour
 {
     [SerializeField] float steerSpeed = 0.1f;
     [SerializeField] public float moveSpeed = 0.001f;
-    private GameObject package;
+    [SerializeField] TextMeshProUGUI timerDisplay;
+    int timeCount;
+    
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         
     }
@@ -16,11 +19,15 @@ public class DriverController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        timeCount = (int)Time.time;
+
+        timerDisplay.text = "Timer: " + timeCount.ToString() + " Seconds";
         float steerAmount = Input.GetAxis("Horizontal") * steerSpeed;
         float speedAmount = Input.GetAxis("Vertical") * moveSpeed;
         transform.Rotate(0, 0, -steerAmount);
         transform.Translate(0, speedAmount, 0);
+        
+
     }
 
 
